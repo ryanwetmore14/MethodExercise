092da7881395957abaf36b2d1e348bf14ad6d43c
ryan wetmore <ryanwetmore1014@gmil.com>
2024-04-29 12:09:05 -0400

    Finished main method

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+
+namespace MethodExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi {userName}. What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine($"{color} Is the best color to exist, What is your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("Cool! now which cars do you enjoy the most!");
+            string car = Console.ReadLine();
+        }
+    }
+}
